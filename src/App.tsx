51f4b6200d687a8b22d3a052718f4fd498cbe1c4
Yuanlin Lin <im.yuanlinlin@gmail.com>
2020-05-27 22:42:23 +0800

    add remove button

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,34 @@ import React, { Component } from "react";
 import "./App.css";
 import html2canvas from "html2canvas";
 import FileSaver from "file-saver";
+import { resolve } from "dns";
 
 interface State {
     cols: string[];
     sizes: string[];
+    priting: boolean;
 }
 
 class App extends Component<{}, State> {
     constructor(props: Readonly<{}>) {
         super(props);
         this.state = {
-            cols: ["衣長", "肩寬", "胸圍", "袖長"],
+            cols: ["衣長", "衣長", "衣長", "衣長"],
             sizes: ["S", "M", "L", "XL"],
+            priting: false,
         };
     }
 
-    print = () => {
-        const input = document.getElementById("table");
-        if (input !== null)
-            html2canvas(input).then((canvas) => {
-                const imgData = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-                FileSaver.saveAs(imgData, "尺寸表.jpg");
-            });
+    print = async () => {
+        this.setState({ priting: true }, () => {
+            const input = document.getElementById("table");
+            if (input !== null)
+                html2canvas(input, { scrollY: 0 }).then((canvas) => {
+                    const imgData = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+                    FileSaver.saveAs(imgData, "尺寸表.jpg");
+                    this.setState({ priting: false });
+                });
+        });
     };
 
     render() {
@@ -31,9 +37,11 @@ class App extends Component<{}, State> {
             <div>
                 <button onClick={() => this.setState({ sizes: [...this.state.sizes, "新尺寸"] })}>增加尺寸</button>
                 <button onClick={() => this.setState({ cols: [...this.state.cols, "新欄位"] })}>新增欄位</button>
+                <button onClick={() => this.print()}>保存圖片</button>
                 <div style={{ marginTop: 36, marginBottom: 36, padding: 24 }} id="table">
+                    <input defaultValue="測量單位：cm (公分)、kg(公斤)" style={{ width: "90%" }} />
                     <table>
-                        <tr>
+                        <tr style={{ lineHeight: 0.5 }}>
                             <td className="colored-background">尺寸</td>
                             {this.state.cols.map((col, index) => (
                                 <td className="colored-background">
@@ -46,12 +54,26 @@ class App extends Component<{}, State> {
                                             this.setState({ cols: newcols });
                                         }}
                                     />
+                                    {this.state.priting ? (
+                                        <p />
+                                    ) : (
+                                        <button
+                                            onClick={() => {
+                                                var newcols = [];
+                                                newcols = this.state.cols;
+                                                newcols.splice(index, 1);
+                                                this.setState({ cols: newcols });
+                                            }}
+                                        >
+                                            刪除
+                                        </button>
+                                    )}
                                 </td>
                             ))}
                         </tr>
                         {this.state.sizes.map((size, index) => (
-                            <tr>
-                                <td className="colored-background">
+                            <tr style={{ lineHeight: 0.2 }}>
+                                <td className="colored-background" style={{ width: 300 }}>
                                     <input
                                         value={size}
                                         onChange={(e) => {
@@ -61,6 +83,20 @@ class App extends Component<{}, State> {
                                             this.setState({ sizes: newsizes });
                                         }}
                                     ></input>
+                                    {this.state.priting ? (
+                                        <p />
+                                    ) : (
+                                        <button
+                                            onClick={() => {
+                                                var newsizes = [];
+                                                newsizes = this.state.sizes;
+                                                newsizes.splice(index, 1);
+                                                this.setState({ sizes: newsizes });
+                                            }}
+                                        >
+                                            刪除
+                                        </button>
+                                    )}
                                 </td>
                                 {this.state.cols.map(() => (
                                     <td>
@@ -70,8 +106,8 @@ class App extends Component<{}, State> {
                             </tr>
                         ))}
                     </table>
+                    <input defaultValue="因測量方法不同，2-3cm 誤差屬正常範圍" style={{ width: "90%" }} />
                 </div>
-                <button onClick={() => this.print()}>保存圖片</button>
             </div>
         );
     }
